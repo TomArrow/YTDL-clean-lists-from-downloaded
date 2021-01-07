3387acea1862ebc4e5c420b3ce7164d89fae9b92
Tom Arrow <davorfilms@googlemail.com>
2021-01-07 12:10:14 +0100

    Add project files.

---
+++ YTDL-clean-lists-from-downloaded/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YTDL_clean_lists_from_downloaded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
